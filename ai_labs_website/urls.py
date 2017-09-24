419124b5de5c9093893fc8cb3f865d1278c6f93a
Dean Armada <deanarmada@Deans-MacBook.local>
2017-09-24 23:59:36 +0800

    Added meetup event page

--- ai_labs_website/urls.py
+++ ai_labs_website/urls.py
@@ -15,18 +15,30 @@ Including another URLconf
 """
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.sitemaps.views import sitemap
 from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 
 from . views import *
+from . sitemaps import BlogSiteMap, CategorySiteMap
+
+
+sitemaps = {
+    'blogs': BlogSiteMap(),
+    # 'category': CategorySiteMap(),
+}
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', home, name='home'),
     url(r'^email/$', email, name='email'),
     url(r'^blogs/', include('blogs.urls')),
+    url(r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap',
+        {'sitemaps': sitemaps}),
+    url(r'^robots\.txt', include('robots.urls')),
+    url(r'^meetup/events/', meetup_events),
 ]
 
 # activating media urls
